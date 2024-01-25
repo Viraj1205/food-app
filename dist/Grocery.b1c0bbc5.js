b13804d0710a4dab13a70c79e534dcabf2e0294c
viraj <virajshinde1298@gmail.com>
2024-01-25 22:58:31 +0530

    fit

---
+++ dist/Grocery.b1c0bbc5.js
@@ -0,0 +1,3 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire77dd;e.register("loR3J",function(o,i){Object.defineProperty(o.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(o.exports,"default",{get:()=>d,set:void 0,enumerable:!0,configurable:!0});var r=e("ayMG0"),d=()=>/*#__PURE__*/(0,r.jsx)("div",{children:/*#__PURE__*/(0,r.jsx)("h1",{children:"This is our grocery store."})})});//# sourceMappingURL=Grocery.b1c0bbc5.js.map
+
+//# sourceMappingURL=Grocery.b1c0bbc5.js.map
